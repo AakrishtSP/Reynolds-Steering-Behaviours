fd65fe84c2d8a9f34033e43e5564051227867c30
nyanko3434 <khusunayan@gmail.com>
2025-02-18 14:34:02 +0545

    Enhance randomVelocity function to accept variable speed; update usages in Agent and States

--- src/Core/Utilities/Utils.cpp
+++ src/Core/Utilities/Utils.cpp
@@ -1,14 +1,21 @@
 #include "pch.h"
 #include "Utils.h"
 
-glm::vec2 randomVelocity(const float requiredSpeed)
+glm::vec2 randomVelocity(const float requiredSpeed, int notConstant = 0)
 {
     static std::random_device rd;
     static std::mt19937 gen(rd());
     static std::uniform_real_distribution<float> angle_dist(0.0f, 2.0f * 3.1415);
 
     const float angle = angle_dist(gen); // Random angle in [0, 2 pi]
-
-    const glm::vec2 velocity = glm::vec2(glm::cos(angle), glm::sin(angle)) * requiredSpeed;
+    float returnedSpeed = requiredSpeed;
+    if (notConstant)
+    {
+        //make requiredSpeed 0 to given value
+        static std::uniform_real_distribution<float> speed_dist(0.0f, requiredSpeed);
+        returnedSpeed = speed_dist(gen);
+        
+    }
+    const glm::vec2 velocity = glm::vec2(glm::cos(angle), glm::sin(angle)) * returnedSpeed;
     return velocity;
 }
